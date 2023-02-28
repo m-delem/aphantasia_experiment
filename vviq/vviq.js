234003ba4bd09b5be443971bc80f5e6ab2b373dd
Maël Delem <mael.a.delem@gmail.com>
2023-02-28 14:29:45 +0100

    vviq clean up

--- vviq/vviq.js
+++ vviq/vviq.js
@@ -1,24 +1,12 @@
-jatos.onLoad(function () { 
+jatos.onLoad(function () {
     const survey = new Survey.Model(json);
     survey.locale = "fr";
     survey
         .onComplete
         .add(
-            function (result) {
-                $("#vviq").hide(); // Hides the 'Thank you for completing ...' message
-                jatos.startNextComponent(result.data);
+            function () {
+                $("#vviq").hide(); // Hides completion message
+                jatos.startNextComponent(survey.data);
             });
-    // Instantiate Showdown
-    const converter = new showdown.Converter();
-    survey.onTextMarkdown.add(function (survey, options) {
-    // Convert Markdown to HTML
-    let str = converter.makeHtml(options.text);
-    // Remove root paragraphs <p></p>
-    str = str.substring(3);
-    str = str.substring(0, str.length - 4);
-    // Set HTML markup to render
-    options.html = str;
-    });
-    $("#vviq").Survey({model: survey});
-    jatos.addAbortButton();
+    $("#vviq").Survey({ model: survey });
 });
