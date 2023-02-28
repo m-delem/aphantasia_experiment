1ee20663803da04051b235b0065823cb832dfe2e
Maël Delem <mael.a.delem@gmail.com>
2023-02-28 17:56:39 +0100

    Update sri_json.js

--- sri_intro/sri_json.js
+++ sri_intro/sri_json.js
@@ -6,14 +6,13 @@ const json = {
         {
           "type": "html",
           "name": "intro_sri",
-          "html": "<h2>Vous allez d&eacute;sormais passer une t&acirc;che de raisonnement spatial.</h2>\n<p>Ce questionnaire comporte 30 questions au total, toutes &agrave; choix multiple.</p>"
+          "html": "<div data-bind=\"html: question.locHtml.koRenderedHtml, css: question.koCss().root\">\n<div data-bind=\"html: question.locHtml.koRenderedHtml, css: question.koCss().root\">\n<h2>Vous allez d&eacute;sormais passer une t&acirc;che de raisonnement spatial.</h2>\n<p>Ce questionnaire comporte 30 questions au total, toutes &agrave; choix multiple, avec une seule r&eacute;ponse correcte.</p>\n<h4>Vous serez limit&eacute;.e &agrave; 30 secondes pour r&eacute;pondre &agrave; chacune d'entre elles. Essayez de r&eacute;pondre rapidement, tout en restant le plus pr&eacute;cis possible.</h4>\n</div>\n</div>"
         }
       ]
     },
   ],
   "showQuestionNumbers": "off",
   "showPrevButton": false,
-  "startSurveyText": "Commencer",
   "completeText": "Commencer",
   "widthMode": "static"
 }
