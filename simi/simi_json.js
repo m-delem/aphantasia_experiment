c054148b2b4b0548e2b1a7208f44c340a81caafc
Maël Delem <mael.a.delem@gmail.com>
2023-03-01 15:27:53 +0100

    Update simi_json.js

--- simi/simi_json.js
+++ simi/simi_json.js
@@ -7,91 +7,91 @@ const json = {
       "elements": [
         {
           "type": "comment",
-          "name": "simili_1",
+          "name": "simili_4",
           "title": "En quoi JAUNE et BLEU se ressemblent ?",
           "hideNumber": true
         },
         {
           "type": "comment",
-          "name": "simili_2",
+          "name": "simili_5",
           "title": "En quoi PIANO et TAMBOUR se ressemblent ?",
           "hideNumber": true
         },
         {
           "type": "comment",
-          "name": "simili_3",
+          "name": "simili_6",
           "title": "En quoi POÈME et STATUE se ressemblent ?",
           "hideNumber": true
         },
         {
           "type": "comment",
-          "name": "simili_4",
+          "name": "simili_7",
           "title": "En quoi BOURGEON et BÉBÉ se ressemblent ?",
           "hideNumber": true
         },
         {
           "type": "comment",
-          "name": "simili_5",
+          "name": "simili_8",
           "title": "En quoi MIEL et LAIT se ressemblent ?",
           "hideNumber": true
         },
         {
           "type": "comment",
-          "name": "simili_6",
+          "name": "simili_9",
           "title": "En quoi NOURRITURE et CARBURANT se ressemblent ?",
           "hideNumber": true
         },
         {
           "type": "comment",
-          "name": "simili_7",
+          "name": "simili_10",
           "title": "En quoi CUBE et CYLINDRE se ressemblent ?",
           "hideNumber": true
         },
         {
           "type": "comment",
-          "name": "simili_8",
+          "name": "simili_11",
           "title": "En quoi NEZ et LANGUE se ressemblent ?",
           "hideNumber": true
         },
         {
           "type": "comment",
-          "name": "simili_9",
+          "name": "simili_12",
           "title": "En quoi SOIE et LAINE se ressemblent ?",
           "hideNumber": true
         },
         {
           "type": "comment",
-          "name": "simili_10",
+          "name": "simili_13",
           "title": "En quoi ÉOLIENNE et BARRAGE se ressemblent ?",
           "hideNumber": true
         },
         {
           "type": "comment",
-          "name": "simili_11",
+          "name": "simili_14",
           "title": "En quoi ÉPHÉMÈRE et PERMANENT se ressemblent ?",
           "hideNumber": true
         },
         {
           "type": "comment",
-          "name": "simili_12",
+          "name": "simili_15",
           "title": "En quoi INONDATION et SÉCHERESSE se ressemblent ?",
           "hideNumber": true
         },
         {
           "type": "comment",
-          "name": "simili_13",
+          "name": "simili_16",
           "title": "En quoi SÉDENTAIRE et NOMADE se ressemblent ?",
           "hideNumber": true
         },
         {
           "type": "comment",
-          "name": "simili_14",
+          "name": "simili_17",
           "title": "En quoi AUTORISER et INTERDIRE se ressemblent ?",
           "hideNumber": true
         },
         {
           "type": "comment",
-          "name": "simili_15",
+          "name": "simili_18",
           "title": "En quoi RÉALITÉ et RÊVE se ressemblent ?",
           "hideNumber": true
         }
