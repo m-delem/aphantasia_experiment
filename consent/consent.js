b882dffb52745a72b23510570f0e158ef7849c49
Maël Delem <mael.a.delem@gmail.com>
2023-03-01 16:43:03 +0100

    update consent

--- consent/consent.js
+++ consent/consent.js
@@ -1,14 +1,16 @@
-jatos.onLoad(function () { 
+jatos.onLoad(function () {
     const survey = new Survey.Model(json);
     survey.locale = "fr";
     survey
         .onComplete
         .add(
-            function (result) {
-                $("#consent").hide(); // Hides the 'Thank you for completing ...' message
-                jatos.startNextComponent(result.data);
+            function () {
+                $("#consent").hide(); // Hides completion message
+                jatos.startNextComponent(
+                    '{"nom":"' + survey.data["nom"] +
+                    '_' + survey.data["prenom"] + '"}'
+                );
             });
-    $("#consent").Survey({model: survey});
-    jatos.addAbortButton();
+    $("#consent").Survey({ model: survey });
 });
 
