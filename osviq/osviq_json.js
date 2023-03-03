378e5cd65f104d478d5f0008da3b541531edf7e0
Maël Delem <mael.a.delem@gmail.com>
2023-03-03 17:24:00 +0100

    Update osviq_json.js

--- osviq/osviq_json.js
+++ osviq/osviq_json.js
@@ -26,43 +26,43 @@ const json = {
           }],
           "rows": [
             {
-              "value": "osviq_1",
+              "value": "osviq_s_1",
               "text": "J'étais très bon en géométrie 3D quand j'étais étudiant."
             },
             {
-              "value": "osviq_2",
+              "value": "osviq_v_2",
               "text": "J'ai des difficultés à m'exprimer par écrit."
             },
             {
-              "value": "osviq_3",
+              "value": "osviq_s_3",
               "text": "Si l'on me demandait de choisir entre les professions et les arts visuels, je préférerais l'ingénierie."
             },
             {
-              "value": "osviq_4",
+              "value": "osviq_v_4",
               "text": "En raison de mes aptitudes verbales, il me serait relativement facile de faire carrière dans les arts du langage."
             },
             {
-              "value": "osviq_5",
+              "value": "osviq_s_5",
               "text": "L'architecture m'intéresse plus que la peinture."
             },
             {
-              "value": "osviq_6",
+              "value": "osviq_o_6",
               "text": "Mes images sont très colorées et lumineuses."
             },
             {
-              "value": "osviq_7",
+              "value": "osviq_s_7",
               "text": "Je préfère les schémas et les croquis quand je lis un manuel plutôt que des illustrations colorées et picturales."
             },
             {
-              "value": "osviq_8",
+              "value": "osviq_v_8",
               "text": "Je raconte des blagues et des histoires mieux que la plupart des gens."
             },
             {
-              "value": "osviq_9",
+              "value": "osviq_v_9",
               "text": "La rédaction d'un essai est difficile pour moi et je n'aime pas du tout le faire."
             },
             {
-              "value": "osviq_10",
+              "value": "osviq_s_10",
               "text": "Mes images sont plutôt des représentations schématiques des choses et des événements plutôt que des images détaillées."
             }],
           "alternateRows": true,
@@ -95,43 +95,43 @@ const json = {
           }],
           "rows": [
             {
-              "value": "osviq_11",
+              "value": "osviq_o_11",
               "text": "Lorsque je lis une fiction, je me fais généralement une image mentale claire et détaillée d'une scène ou d'une pièce décrite."
             },
             {
-              "value": "osviq_12",
+              "value": "osviq_o_12",
               "text": "Si l'on me demandait de choisir entre les professions d'ingénieur ou les arts visuels, je choisirais les arts visuels."
             },
             {
-              "value": "osviq_13",
+              "value": "osviq_o_13",
               "text": "J'ai une mémoire photographique."
             },
             {
-              "value": "osviq_14",
+              "value": "osviq_s_14",
               "text": "Je peux facilement imaginer et tourner mentalement des figures géométriques tridimensionnelles."
             },
             {
-              "value": "osviq_15",
+              "value": "osviq_o_15",
               "text": "J'aime les images aux couleurs vives et aux formes inhabituelles, comme celles de l'art moderne."
             },
             {
-              "value": "osviq_16",
+              "value": "osviq_v_16",
               "text": "Mes compétences verbales sont excellentes."
             },
             {
-              "value": "osviq_17",
+              "value": "osviq_s_17",
               "text": "Lorsque je pense à un concept abstrait (ou à un bâtiment), j'imagine dans mon esprit un bâtiment schématique abstrait ou son plan plutôt qu'un bâtiment concret spécifique."
             },
             {
-              "value": "osviq_18",
+              "value": "osviq_o_18",
               "text": "Lorsque j'entre dans un magasin familier pour acheter un article spécifique, je peux facilement imaginer l'emplacement exact de l'article cible, l'étagère sur laquelle il se trouve, comment il est disposé et les articles environnants."
             },
             {
-              "value": "osviq_19",
+              "value": "osviq_v_19",
               "text": "Assembler des meubles en kit (par exemple, un meuble de télévision ou une chaise) est beaucoup plus facile pour moi lorsque je dispose d'instructions verbales détaillées que lorsque je n'ai qu'un diagramme ou une image."
             },
             {
-              "value": "osviq_20",
+              "value": "osviq_o_20",
               "text": "Mes images mentales sont très vivantes et photographiques."
             },
           ],
@@ -165,43 +165,43 @@ const json = {
           }],
           "rows": [
             {
-              "value": "osviq_21",
+              "value": "osviq_v_21",
               "text": "Pour expliquer quelque chose, je préfère donner des explications verbales que de faire des dessins ou des croquis."
             },
             {
-              "value": "osviq_22",
+              "value": "osviq_v_22",
               "text": "Si quelqu'un me donne des nombres à deux chiffres à additionner (par ex. 43 et 32) je ferais simplement l'addition sans visualiser les chiffres."
             },
             {
-              "value": "osviq_23",
+              "value": "osviq_o_23",
               "text": "Mes images mentales de différents objets ressemblent beaucoup à la taille, la forme et la couleur des objets réels que j'ai vus."
             },
             {
-              "value": "osviq_24",
+              "value": "osviq_v_24",
               "text": "En général, je n'essaie pas de visualiser ou d'esquisser des diagrammes lorsque je lis un manuel."
             },
             {
-              "value": "osviq_25",
+              "value": "osviq_s_25",
               "text": "Normalement, je n'ai pas beaucoup d'images spontanées ; je me sers de mes images mentales principalement lorsque j'essaie de résoudre certains problèmes (mathématiques par exemple)."
             },
             {
-              "value": "osviq_26",
+              "value": "osviq_o_26",
               "text": "Lorsque j'imagine le visage d'un ami, j'ai une image parfaitement claire et lumineuse."
             },
             {
-              "value": "osviq_27",
+              "value": "osviq_s_27",
               "text": "J'ai d'excellentes capacités en graphisme technique."
             },
             {
-              "value": "osviq_28",
+              "value": "osviq_v_28",
               "text": "Lorsque je me souviens d'une scène, j'utilise des descriptions verbales plutôt que des images mentales."
             },
             {
-              "value": "osviq_29",
+              "value": "osviq_o_29",
               "text": "Je peux facilement me souvenir d'un grand nombre de détails visuels que quelqu'un d'autre ne remarquerait jamais. Par exemple, je peux me souvenir automatiquement de certaines choses, comme la couleur de la chemise que portait une personne ou la couleur de ses chaussures."
             },
             {
-              "value": "osviq_30",
+              "value": "osviq_s_30",
               "text": "Je peux facilement dessiner le plan d'un bâtiment que je connais bien."
             },
 
@@ -236,43 +236,43 @@ const json = {
           }],
           "rows": [
             {
-              "value": "osviq_31",
+              "value": "osviq_s_31",
               "text": "À l'école, je n'avais aucun problème avec la géométrie."
             },
             {
-              "value": "osviq_32",
+              "value": "osviq_s_32",
               "text": "Je suis doué pour les jeux spatiaux qui consistent à construire à partir de blocs et de papier (par exemple Lego, Tetris, Origami)."
             },
             {
-              "value": "osviq_33",
+              "value": "osviq_o_33",
               "text": "Parfois, mes images sont si vives et persistantes qu'il est difficile de les ignorer."
             },
             {
-              "value": "osviq_34",
+              "value": "osviq_o_34",
               "text": "Je peux fermer les yeux et me représenter facilement une scène que j'ai vécue."
             },
             {
-              "value": "osviq_35",
+              "value": "osviq_v_35",
               "text": "J'ai une aisance supérieure à la moyenne dans l'utilisation des mots."
             },
             {
-              "value": "osviq_36",
+              "value": "osviq_v_36",
               "text": "Je préfère avoir une description verbale d'un objet ou d'une personne ou d'une personne plutôt qu'une image."
             },
             {
-              "value": "osviq_37",
+              "value": "osviq_v_37",
               "text": "Je suis toujours attentif à la structure des phrases."
             },
             {
-              "value": "osviq_38",
+              "value": "osviq_s_38",
               "text": "Mes images sont plus schématiques que colorées et picturales."
             },
             {
-              "value": "osviq_39",
+              "value": "osviq_v_39",
               "text": "J'aime pouvoir reformuler mes pensées de plusieurs façons pour le plaisir de varier, à l'écrit comme à l'oral."
             },
             {
-              "value": "osviq_40",
+              "value": "osviq_o_40",
               "text": "Je me souviens de tout visuellement. Je peux raconter ce que les gens portaient à un dîner, je peux parler de la façon dont ils étaient assis et de ce à quoi ils ressemblaient, probablement plus en détail que je ne pourrais discuter de ce qu'ils ont dit."
             },
 
@@ -307,23 +307,23 @@ const json = {
           }],
           "rows": [
             {
-              "value": "osviq_41",
+              "value": "osviq_v_41",
               "text": "J'ai parfois du mal à exprimer exactement ce que je veux dire."
             },
             {
-              "value": "osviq_42",
+              "value": "osviq_s_42",
               "text": "J'ai du mal à imaginer ce à quoi ressemblerait exactement une figure géométrique tridimensionnelle lorsqu'elle est tournée."
             },
             {
-              "value": "osviq_43",
+              "value": "osviq_o_43",
               "text": "Mes images visuelles sont tout le temps dans ma tête. Elles sont juste-là."
             },
             {
-              "value": "osviq_44",
+              "value": "osviq_s_44",
               "text": "Mes capacités graphiques me permettraient de faire une carrière en architecture assez facilement."
             },
             {
-              "value": "osviq_45",
+              "value": "osviq_o_45",
               "text": "Lorsque j'entends un annonceur de radio ou un DJ que je n'ai jamais vu, je me surprends généralement à imaginer à quoi il ou elle pourrait ressembler."
             },
 
